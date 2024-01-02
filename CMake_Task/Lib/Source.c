0b73fb69b2c42c853b1b7e52c395e6edae7516fd
Mina <mina5ela14@gmail.com>
2024-01-02 16:49:21 +0200

    Task5 EmbeddedLinux Added

---
+++ CMake_Task/Lib/Source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int ADD(int x , int y)
+{
+  printf("Addition Result = %d\n",x+y);
+}
+
+int DEV(int x , int y)
+{
+printf("Devision Result = %d\n",x/y);
+}
+
+int MOD(int x , int y)
+{
+printf("Modolus Result = %d\n",x%y);
+}
+
+
+int MULTIPLY(int x , int y)
+{
+printf("Multiplication Result = %d\n",x*y);
+}
+
+
+int SUB(int x , int y)
+{
+printf("Submition Result = %d\n",x-y);
+}
+
+
